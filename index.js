2624a7e0fd03fbd2a5a685a4a6341b0090f4220b
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2024-06-10 18:54:50 +0000

    Added Leetcode Shuffle Array (easy)

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-/*
 
-1. Two Sum
+//For each problem: go to leetcode for all directions and examples
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//1. Two Sum (Easy) --arrays and lists
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
-Example 1:
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
-
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
-*/
-
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 var twoSum = function (nums, target) {
   let map = new Map();
 
@@ -38,9 +13,32 @@ var twoSum = function (nums, target) {
     if (map.has(num2)) {
       return [i, map.get(num2)];
     } else {
-      map.set(nums[i], i); //the value goes first??
+      map.set(num1, i); //the value goes first??
     }
   }
 };
 
+//check method: 
 console.log(twoSum([2,7,11,15], 9));
+
+
+/*----------------------------------------------------
+      2. Shuffle Array (Easy) --arrays and lists
+*/
+
+var shuffle = function(nums, n) {
+   newArray = [];
+for(let i = 0; i < n; i++){
+ 
+  newArray.push(nums[i]);
+  newArray.push(nums[i + n]);
+  
+}
+  return newArray; 
+};
+
+console.log(shuffle([1,2,3,4,4,3,2,1], 4))
+
+/* -----------------------------------------------
+      3. 
+*/
